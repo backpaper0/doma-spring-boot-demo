68e9e1c1df7903e17d9fccb3e651c40d99743583
Uragami Taichi <backpaper0@gmail.com>
2021-11-13 21:09:02 +0900

    Update module-info.java

--- src/main/java/com/example/module-info.java
+++ src/main/java/com/example/module-info.java
@@ -1,15 +1,15 @@
 module com.example.app {
+    requires doma.spring.boot.autoconfigure;
+    requires doma.spring.boot.core;
     requires java.annotation;
+    requires java.instrument;
     requires org.seasar.doma.core;
     requires org.seasar.doma.processor;
-    requires spring.boot;
-    requires spring.boot.autoconfigure;
     requires spring.beans;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
     requires spring.context;
     requires spring.tx;
     requires spring.web;
-    requires org.seasar.doma.boot.autoconfigure;
-    requires org.seasar.doma.boot.core;
-    requires java.instrument;
     opens com.example;
 }
