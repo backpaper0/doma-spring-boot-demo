6409e7a9fb34be5b2c380aa0fd32c664cb63863f
Uragami Taichi <backpaper0@gmail.com>
2022-11-25 05:30:45 +0900

    Bump Doma, doma-spring-boot, Spring Boot

--- src/main/java/com/example/ReservationDao.java
+++ src/main/java/com/example/ReservationDao.java
@@ -1,23 +1,23 @@
 package com.example;
 
+import java.util.List;
+
 import org.seasar.doma.Dao;
 import org.seasar.doma.Insert;
 import org.seasar.doma.Select;
 import org.seasar.doma.boot.ConfigAutowireable;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @ConfigAutowireable
 @Dao
 public interface ReservationDao {
-    @Select
-    List<Reservation> selectAll();
+	@Select
+	List<Reservation> selectAll();
 
-    @Select
-    List<Reservation> selectByName(String name);
+	@Select
+	List<Reservation> selectByName(String name);
 
-    @Insert
-    @Transactional
-    int insert(Reservation reservation);
+	@Insert
+	@Transactional
+	int insert(Reservation reservation);
 }
